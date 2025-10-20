c79f865266f63f2797500495265aa29db377551b
LanNT - Dev <113458632+LanNT-Dev@users.noreply.github.com>
2025-10-20 16:34:00 +0700

    send OTP

--- src/main.ts
+++ src/main.ts
@@ -29,7 +29,6 @@ async function bootstrap() {
 
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api/docs', app, document);
-
   await app.listen(process.env.PORT ?? 443);
   Logger.log(`Server run at port ${process.env.PORT ?? 3000} 😍😍😍`, 'Server');
 }
