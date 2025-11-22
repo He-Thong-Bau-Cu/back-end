467abe5bd14c692ee32f464341df1949a00b1f12
LanNT - Dev <113458632+LanNT-Dev@users.noreply.github.com>
2025-11-22 23:03:47 +0700

    create api statistic secretary

--- src/modules/statistics/statistics.service.ts
+++ src/modules/statistics/statistics.service.ts
@@ -225,4 +225,27 @@ export class StatisticsService {
     }
   }
 
+  //Thống kế cho thư kí
+  async getSecretaryDashboard(electionId: string, userId: string) {
+    //tổng số voter tham giao election
+    const totalVoters = await this.votersModel.countDocuments({
+      electionId: new Types.ObjectId(electionId)
+    });
+
+    //Tống số ủy quyền chờ phê duyệt
+    const totalConfirmed = await this.delegationsModel.countDocuments({ status: STATUS.CONFIRMED });
+
+    //Tổng số cuộc bầu cử user tham gia
+    const totalElectionsParticipated = await this.participantsModel.countDocuments({
+      userId: new Types.ObjectId(userId),
+    });
+
+    return {
+      totalVoters,
+      totalConfirmed,
+      totalElectionsParticipated,
+    };
+
+  }
+
 }
